9594721aa29e41aa1efbf36aa08097b058683d02
Gotve <akselvolkov40@gmail.com>
2026-01-18 11:09:13 +0500

    Removed default piece of code, Changed mod version from 1.0-SNAPSHOT to 1.0.1, Filled mod_authors and mod_description fields.properties, Changed mod name from "Firma_Skies_Core" to "Firma Skies core".

--- src/main/java/com/gotve/firma_skies_core/mixin/create/BoilerDataMixin.java
+++ src/main/java/com/gotve/firma_skies_core/mixin/create/BoilerDataMixin.java
@@ -9,6 +9,7 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
+@SuppressWarnings("removal")
 @Mixin(value = BoilerData.BoilerFluidHandler.class, remap = false)
 public class BoilerDataMixin {
 
